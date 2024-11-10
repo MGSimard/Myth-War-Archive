f43783857431b19c6c229c968c3a13f5ff8373d4
MGSimard <g.marcgs@gmail.com>
2024-11-10 14:08:59 -0500

    Desktop layout

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,22 @@
+import Link from "next/link";
+
 export function Nav() {
   return (
     <nav>
       <img src="#" alt="Logo" />
       <ul>
-        <li>L1</li>
-        <li>L2</li>
-        <li>L3</li>
-        <li>L4</li>
+        <li>
+          <Link href="#">L1</Link>
+        </li>
+        <li>
+          <Link href="#">L2</Link>
+        </li>
+        <li>
+          <Link href="#">L3</Link>
+        </li>
+        <li>
+          <Link href="#">L4</Link>
+        </li>
       </ul>
     </nav>
   );
