2477cf571c3f690302e0cb7dfe5497ae59d650fc
MGSimard <g.marcgs@gmail.com>
2024-11-11 15:02:42 -0500

    Set up uploadthing remotePattern

--- next.config.js
+++ next.config.js
@@ -6,8 +6,18 @@ import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  devIndicators: {
-    appIsrStatus: false,
+  // devIndicators: {
+  //   appIsrStatus: false,
+  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+        pathname: "/a/ydpng72sbw/**",
+      },
+    ],
   },
   // typescript: {
   //   ignoreBuildErrors: true,
--- src/app/world/page.tsx
+++ src/app/world/page.tsx
@@ -2,6 +2,10 @@ export default function Page() {
   return (
     <>
       <h2>World</h2>
+      <img
+        src="https://utfs.io/f/e0BHXYWYpV1Nv1uQCPKwjQM0IfTDPJYum2ihyFXEaCKqn7xN"
+        alt="World map of the continent of Rarus."
+      />
       <p>Add stuff</p>
     </>
   );
