154ff0bc0d2887f08da5b32ee87d833f5b3b0425
MGSimard <g.marcgs@gmail.com>
2024-11-10 21:23:57 -0500

    Few more tests

--- src/app/world/page.tsx
+++ src/app/world/page.tsx
@@ -6,7 +6,25 @@ export default function Page() {
       </header>
       <main className="panel">
         <section>
-          <h2>Section 1</h2>
+          <div className="section-header">
+            <div className="sh-title">
+              <img src="#" alt="O" />
+              <h2>Section 1</h2>
+            </div>
+            <div className="sh-topright">More &gt;&gt;</div>
+          </div>
+          <div className="section-content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut tellus sit amet orci lobortis tincidunt.
+              Maecenas ultrices condimentum massa, vitae malesuada neque aliquam id. Nulla justo ipsum, hendrerit eu
+              consectetur ut, hendrerit id dolor. Aenean aliquam consectetur nibh, eu blandit sapien pulvinar id. Etiam
+              non congue ex, sit amet maximus sapien. Nullam eu nunc porta, condimentum sapien et, condimentum orci.
+              Duis id porta libero. Pellentesque fringilla et lorem a feugiat. Curabitur vel libero a sapien vulputate
+              porta eu in sem. Donec pellentesque augue viverra rhoncus ultricies. Vivamus fermentum eu dolor ac
+              viverra. Nulla ultricies justo quam, quis scelerisque quam pulvinar ac. Etiam nisl libero, ultrices ac
+              sodales et, lobortis ac felis.
+            </p>
+          </div>
         </section>
         <section>
           <h2>Section 2</h2>
