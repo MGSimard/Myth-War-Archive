10f2b605daff37c8349309e4109452af0ace529e
MGSimard <g.marcgs@gmail.com>
2024-11-10 21:37:44 -0500

    Start logo stuff

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import { NavLink } from "@/components/NavLink";
 import { IconMap, IconTools, IconGroup, IconAssets } from "@/components/Icons";
 
 export function Nav() {
   return (
     <nav>
-      <img src="#" alt="Logo" />
+      <Link href="/">
+        <img src="#" alt="Logo" className="nav-logo" />
+      </Link>
       <ul>
         <NavLink path="World" icon={<IconMap />} />
         <NavLink path="Assets" icon={<IconAssets />} />
