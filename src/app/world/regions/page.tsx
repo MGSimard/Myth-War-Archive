900cedc73d306f26d81cf583e3091412ce038e19
MGSimard <g.marcgs@gmail.com>
2024-11-13 20:27:38 -0500

    Start working out some layout for regions page, think about using full 8kx4k maps and stuff rendered only on click as lightboxes with drag & zoom?

--- src/app/world/regions/page.tsx
+++ src/app/world/regions/page.tsx
@@ -2,14 +2,74 @@ export default function Page() {
   return (
     <>
       <h2>Regions</h2>
+
       <hr className="divider" />
-      <h3>Woodlingor</h3>
-      <hr />
-      <h3>Blython</h3>
-      <hr />
-      <h3>Desert City</h3>
-      <hr />
-      <h3>Demon Square</h3>
+
+      <h3 id="sky-passage">Sky Passage</h3>
+      <div className="grid-maps">
+        <figure>
+          <img src="https://utfs.io/f/e0BHXYWYpV1NcPnn5kpm4P0hJ2ERyDWqe9dvOoN5SbMzHniA" alt="" />
+          <figcaption>
+            Map of Sky Passage<span>Myth War Online</span>
+          </figcaption>
+        </figure>
+      </div>
+
+      <hr className="divider" />
+
+      <h3 id="woodlingor">Woodlingor</h3>
+      <div className="grid-maps">
+        <figure>
+          <img src="https://utfs.io/f/e0BHXYWYpV1N9Sn9K0eLJKf8YNVx0pdBSilFb2grkye1hR5Z" alt="" />
+          <figcaption>
+            Map of Woodlingor<span>Myth War Online</span>
+          </figcaption>
+        </figure>
+        <figure>
+          <img src="https://utfs.io/f/e0BHXYWYpV1NHOAuqo3Hz8daoBx7sqZNvfWYUMDPwghmGn4S" alt="" />
+          <figcaption>
+            Map of Woodlingor (Winter)<span>Myth War Online</span>
+          </figcaption>
+        </figure>
+        <figure>
+          <img src="https://utfs.io/f/e0BHXYWYpV1NHVTd543Hz8daoBx7sqZNvfWYUMDPwghmGn4S" alt="" />
+          <figcaption>
+            Map of Woodlingor<span>Myth War II</span>
+          </figcaption>
+        </figure>
+      </div>
+
+      <hr className="divider" />
+
+      <h3 id="blython">Blython</h3>
+      <div className="grid-maps">
+        <figure>
+          <img src="https://utfs.io/f/e0BHXYWYpV1NkLtWrDPqMBg5XUDSJtsO7HKZNbFp8vYiaczu" alt="" />
+          <figcaption>
+            Map of Blython<span>Myth War Online</span>
+          </figcaption>
+        </figure>
+        <figure>
+          <img src="https://utfs.io/f/e0BHXYWYpV1NzkhwoMRFQiEB3JMNWaAZtubXjgTY9rePlV28" alt="" />
+          <figcaption>
+            Map of Blython (Winter)<span>Myth War Online</span>
+          </figcaption>
+        </figure>
+        <figure>
+          <img src="https://utfs.io/f/e0BHXYWYpV1NYDMPDgIU41I3SWdTpuwmvQcPXi8Aj7fFz90t" alt="" />
+          <figcaption>
+            Map of Blython<span>Myth War II</span>
+          </figcaption>
+        </figure>
+      </div>
+
+      <hr className="divider" />
+
+      <h3 id="desert-city">Desert City</h3>
+
+      <hr className="divider" />
+
+      <h3 id="demon-square">Demon Square</h3>
     </>
   );
 }
