3f383bddb8c1e0c7b56f8985b2497ba5a53ef90c
MGSimard <g.marcgs@gmail.com>
2024-11-13 21:21:11 -0500

    Cities added

--- src/app/world/regions/page.tsx
+++ src/app/world/regions/page.tsx
@@ -13,6 +13,12 @@ export default function Page() {
             Map of Sky Passage<span>Myth War Online</span>
           </figcaption>
         </figure>
+        <figure>
+          <img src="https://utfs.io/f/e0BHXYWYpV1NIwF2QmsvXPJ3CktFqy9AHai6V2jzSrf7h5L8" alt="" />
+          <figcaption>
+            Map of Sky Passage<span>Myth War II</span>
+          </figcaption>
+        </figure>
       </div>
 
       <hr className="divider" />
@@ -66,10 +72,37 @@ export default function Page() {
       <hr className="divider" />
 
       <h3 id="desert-city">Desert City</h3>
-
+      <div className="grid-maps">
+        <figure>
+          <img src="https://utfs.io/f/e0BHXYWYpV1NgliGLY7xGMfIyqoH5R2PXCVwv1SWmBx3tnlF" alt="" />
+          <figcaption>
+            Map of Desert City<span>Myth War Online</span>
+          </figcaption>
+        </figure>
+        <figure>
+          <img src="https://utfs.io/f/e0BHXYWYpV1NWIi1cxbWAFCPxQiv7YOXs6Tug2VtZyIdnwH1" alt="" />
+          <figcaption>
+            Map of Desert City<span>Myth War II</span>
+          </figcaption>
+        </figure>
+      </div>
       <hr className="divider" />
 
       <h3 id="demon-square">Demon Square</h3>
+      <div className="grid-maps">
+        <figure>
+          <img src="https://utfs.io/f/e0BHXYWYpV1NUw8Px6fBhVC1bNduT6OStolse3kI7QiMEPKm" alt="" />
+          <figcaption>
+            Map of Demon Square<span>Myth War Online</span>
+          </figcaption>
+        </figure>
+        <figure>
+          <img src="https://utfs.io/f/e0BHXYWYpV1Ne1MHvWYpV1NBWkFjoUeicuC7vEbRTlytzdh0" alt="" />
+          <figcaption>
+            Map of Demon Square<span>Myth War II</span>
+          </figcaption>
+        </figure>
+      </div>
     </>
   );
 }
