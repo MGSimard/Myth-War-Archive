8520234dcef098ffc111841bd885ca54f2c76e5c
MGSimard <g.marcgs@gmail.com>
2024-11-17 00:50:08 -0500

    Add proprietary python tool to directory

--- src/app/tools/modding/page.tsx
+++ src/app/tools/modding/page.tsx
@@ -1,9 +1,37 @@
+import { IconDownload } from "@/components/Icons";
+
 export default function Page() {
   return (
     <>
       <h2>Modding</h2>
       <hr className="divider" />
-      <p>Modding stuff</p>
+      <table className="table-assets">
+        <thead>
+          <tr>
+            <th>Tool</th>
+            <th>Description</th>
+            <th>Download</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Tool</td>
+            <td>Desc</td>
+            <td>
+              <a
+                href="#"
+                download=""
+                target="_blank"
+                rel="noopener noreferrer"
+                className="btn-download"
+                aria-label={`Download ${"TOOLHERE"}`}
+                title={`Download ${"TOOLHERE"}`}>
+                <IconDownload />
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 }
--- src/app/tools/reverse-engineering/page.tsx
+++ src/app/tools/reverse-engineering/page.tsx
@@ -1,9 +1,42 @@
+import toolsRE from "@/data/tools-re.json";
+import { IconDownload } from "@/components/Icons";
+
 export default function Page() {
   return (
     <>
       <h2>Reverse-Engineering</h2>
       <hr className="divider" />
-      <p>Reverse-Engineering stuff</p>
+      <table className="table-assets">
+        <thead>
+          <tr>
+            <th>Tool</th>
+            <th>Author</th>
+            <th>Description</th>
+            <th>Download</th>
+          </tr>
+        </thead>
+        <tbody>
+          {toolsRE.map((tool) => (
+            <tr key={tool.file}>
+              <td>{tool.file}</td>
+              <td>{tool.author}</td>
+              <td>{tool.desc}</td>
+              <td>
+                <a
+                  href={tool.link}
+                  download={tool.file}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="btn-download"
+                  aria-label={`Download ${tool.file}`}
+                  title={`Download ${tool.file}`}>
+                  <IconDownload />
+                </a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
