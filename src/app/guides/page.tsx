c713687eab4043f49298c559b823203f278cf0a8
MGSimard <g.marcgs@gmail.com>
2024-11-25 14:49:22 -0500

    Info

--- src/app/guides/page.tsx
+++ src/app/guides/page.tsx
@@ -3,7 +3,12 @@ export default function Page() {
     <>
       <h2>Guides</h2>
       <hr className="divider" />
-      <p>Guides stuff</p>
+      <h3>Leveling</h3>
+      <p>
+        Contains guides regarding the entire leveling process from start to finish. Includes most efficient options and
+        their alternatives. Within you&apos;ll also find information regarding events which coincide with the leveling
+        process, such as the Wednesday Event.
+      </p>
     </>
   );
 }
--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -3,7 +3,16 @@ export default function Page() {
     <>
       <h2>Tools</h2>
       <hr className="divider" />
-      <p>Tools stuff</p>
+      <h3>Reverse-Engineering</h3>
+      <p>
+        Links and downloads to tools and other utilities meant to aid you in dealing with they game&apos;s core files
+        and assets. For the time being, most of these will be written in Python and therefore will require Python 3.6+
+        to run without issues. All tools should include a README file conmtaining instructions on how to use them.
+        Ideally at some point we move towards executables which don&apos;t require dependencies, building or programming
+        knowledge to utilize. The goal is ease of use and accessibility for all.
+      </p>
+      <h3>Modding</h3>
+      <p>Client modifications which provide quality of life improvements.</p>
     </>
   );
 }
